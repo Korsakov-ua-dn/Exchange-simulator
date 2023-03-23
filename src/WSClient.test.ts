6b77ea12533ec1864e829ef9acbcc46a6cde7e5d
korsakov <korsakov.ua.dn@gmail.com>
2023-03-23 18:23:04 +0300

    refactoring/eliminated duplication of ws server and client connection logic

--- src/WSClient.test.ts
+++ src/WSClient.test.ts
@@ -1,25 +1,25 @@
 import Server from "jest-websocket-mock";
-import WSConnector from "./WSClient";
 import Decimal from "decimal.js";
 
+import WSConnector from "./WSClient";
 import { ClientMessageType, Instrument, ServerMessageType } from "./Enums";
 import { ClientEnvelope, SubscribeMarketData } from "./Models/ClientMessages";
 import { MarketDataUpdate, ServerEnvelope } from "./Models/ServerMessages";
 
 describe("WS", () => {
   let server: Server;
-  beforeEach(() => {
+  let client_1: WSConnector;
+  beforeEach(async () => {
     server = new Server("ws://127.0.0.1:3000/ws/", {
       jsonProtocol: true,
     });
+    client_1 = new WSConnector();
+    client_1.connect();
+    await server.connected;
   });
 
   // ClientMessageType.subscribeMarketData
   it("the server keeps track of received messages, and yields them as they come in", async () => {
-    const client = new WSConnector();
-    client.connect();
-    await server.connected;
-
     const message: SubscribeMarketData = {
       instrument: Instrument.usd_rub
     }
@@ -28,30 +28,27 @@ describe("WS", () => {
       message,
     };
 
-    client.send(request);
+    client_1.send(request);
     await expect(server).toReceiveMessage(request);
     expect(server).toHaveReceivedMessages([request]);
   });
 
   // ServerMessageType.marketDataUpdate
   it("the mock server sends messages to connected clients", async () => {
-    const client1 = new WSConnector();
-    client1.connect()
-    await server.connected;
-    const client2 = new WSConnector();
-    client2.connect()
+    const client_2 = new WSConnector();
+    client_2.connect()
     await server.connected;
 
-    const messages1: ServerEnvelope[] = [];
-    const messages2: ServerEnvelope[] = [];
-    if (typeof client1.connection !== "undefined") {
-      client1.connection.onmessage = (e) => {
-        messages1.push(e.data);
+    const messages_1: ServerEnvelope[] = [];
+    const messages_2: ServerEnvelope[] = [];
+    if (typeof client_1.connection !== "undefined") {
+      client_1.connection.onmessage = (e) => {
+        messages_1.push(e.data);
       };
     }
-    if (typeof client2.connection !== "undefined") {
-      client2.connection.onmessage = (e) => {
-        messages2.push(e.data);
+    if (typeof client_2.connection !== "undefined") {
+      client_2.connection.onmessage = (e) => {
+        messages_2.push(e.data);
       };
     }
 
@@ -71,32 +68,27 @@ describe("WS", () => {
     }
 
     server.send(responce);
-    expect(messages1).toEqual([JSON.stringify(responce)]);
-    expect(messages2).toEqual([JSON.stringify(responce)]);
+    expect(messages_1).toEqual([JSON.stringify(responce)]);
+    expect(messages_2).toEqual([JSON.stringify(responce)]);
   });
 
   // Server error
   it("the mock server sends errors to connected clients", async () => {
-    const client = new WSConnector();
-    client.connect();
-    await server.connected;
-
     let disconnected = false;
     const cb = jest.fn()
-    if (typeof client.connection !== "undefined") {
-      client.connection.onclose = () => {
+    if (typeof client_1.connection !== "undefined") {
+      client_1.connection.onclose = () => {
         disconnected = true;
       };
-      client.connection.onerror = cb;
+      client_1.connection.onerror = cb;
     }
   
     server.error();
     expect(disconnected).toBe(true);
-    expect(cb).toHaveBeenCalled();
-  
   });
 
   afterEach(() => {
     server.close();
+    Server.clean(); // ?
   })
 });
